0da11a7453725eec776aa7b4ce5871065e4a5641
Jake <jakemorey@hotmail.co.uk>
2013-01-27 14:22:09 +0000

    removed some unused files

--- Whirlpool/gen/example/whirlpool/R.java
+++ Whirlpool/gen/example/whirlpool/R.java
@@ -12,24 +12,22 @@ public final class R {
     }
     public static final class drawable {
         public static final int boat=0x7f020000;
-        public static final int diver=0x7f020001;
-        public static final int diver_sprites=0x7f020002;
-        public static final int diver_sprites_2=0x7f020003;
-        public static final int duckdown2=0x7f020004;
-        public static final int duckleftandright2=0x7f020005;
-        public static final int duckup2=0x7f020006;
-        public static final int exitbtn=0x7f020007;
-        public static final int frog=0x7f020008;
-        public static final int frog2=0x7f020009;
-        public static final int frogtemp=0x7f02000a;
-        public static final int ic_action_search=0x7f02000b;
-        public static final int ic_launcher=0x7f02000c;
-        public static final int mainmenubg=0x7f02000d;
-        public static final int optionsbtn=0x7f02000e;
-        public static final int playbtn=0x7f02000f;
-        public static final int shark=0x7f020010;
-        public static final int wateroffset=0x7f020011;
-        public static final int whirlpool=0x7f020012;
+        public static final int diver_sprites=0x7f020001;
+        public static final int diver_sprites_2=0x7f020002;
+        public static final int duckdown2=0x7f020003;
+        public static final int duckleftandright2=0x7f020004;
+        public static final int duckup2=0x7f020005;
+        public static final int exitbtn=0x7f020006;
+        public static final int frog=0x7f020007;
+        public static final int frog2=0x7f020008;
+        public static final int ic_action_search=0x7f020009;
+        public static final int ic_launcher=0x7f02000a;
+        public static final int mainmenubg=0x7f02000b;
+        public static final int optionsbtn=0x7f02000c;
+        public static final int playbtn=0x7f02000d;
+        public static final int shark=0x7f02000e;
+        public static final int wateroffset=0x7f02000f;
+        public static final int whirlpool=0x7f020010;
     }
     public static final class id {
         public static final int exit=0x7f080007;
