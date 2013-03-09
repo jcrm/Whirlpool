a511bc8499ace6f4927a7b251713f689fe539b76
Jake <jakemorey@hotmail.co.uk>
2013-03-09 12:09:47 +0000

    updated to the latest code with the duck starting in the whirlpool
    Signed-off-by: Jake <jakemorey@hotmail.co.uk>

--- Whirlpool/gen/com/sinkingduckstudios/whirlpool/R.java
+++ Whirlpool/gen/com/sinkingduckstudios/whirlpool/R.java
@@ -22,12 +22,11 @@ public final class R {
         public static final int ic_action_search=0x7f020008;
         public static final int ic_launcher=0x7f020009;
         public static final int mainmenu_background=0x7f02000a;
-        public static final int mainmenubg2=0x7f02000b;
-        public static final int options_button=0x7f02000c;
-        public static final int play_button=0x7f02000d;
-        public static final int torpedo_sprites=0x7f02000e;
-        public static final int wateroffset_background=0x7f02000f;
-        public static final int whirlpool_sprites=0x7f020010;
+        public static final int options_button=0x7f02000b;
+        public static final int play_button=0x7f02000c;
+        public static final int torpedo_sprites=0x7f02000d;
+        public static final int wateroffset_background=0x7f02000e;
+        public static final int whirlpool_sprites=0x7f02000f;
     }
     public static final class id {
         public static final int exit=0x7f080007;
