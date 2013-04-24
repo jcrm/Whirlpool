f7f93eaa351daa9004a5cc1ab08ca6aed493e995
Fraser <Fraser@Frasers-PC.home>
2013-04-24 16:51:50 +0100

    More comments, still more to come

--- Whirlpool/src/com/sinkingduckstudios/whirlpool/logic/WPools.java
+++ Whirlpool/src/com/sinkingduckstudios/whirlpool/logic/WPools.java
@@ -1,9 +1,10 @@
 /*
- * Author:
- * Last Updated:
+ * Author: Fraser Tomison
+ * Last Updated: 14/04/13
  * Content:
- * 
- * 
+ * This is the whirlpool model for the game,
+ * It holds all the whirlpool objects and 
+ * interfaces all interactions with them
  */
 package com.sinkingduckstudios.whirlpool.logic;
 
@@ -15,17 +16,22 @@ public class WPools {
     
     private final ArrayList<Whirlpool> wpools = new ArrayList<Whirlpool>();
 
-    /** @return the most recently added dot. */
+    /** @return the most recently added whirlpool. */
     public Whirlpool getLastWpool() {
         return (wpools.size() <= 0) ? null : wpools.get(wpools.size()-1); 
     }
     
-    /** @return immutable list of dots. */
+    /** @return immutable list of whirlpools. */
     public ArrayList<Whirlpool> getWpools(){
     	return wpools;
     }
 
-   
+   /**
+    * Check point for collision with whirlpools
+    * @param x
+    * @param y
+    * @return index of whirlpool, or -1 if none
+    */
     public int checkCollision(float x, float y){
     	x += Constants.getLevel().getScrollBy();
     	for (int i=0; i<wpools.size(); i++)
@@ -35,6 +41,14 @@ public class WPools {
     	return -1;
     }
     
+    /**
+     * User has created a whirlpool
+     * @param x
+     * @param y
+     * @param size depreciated
+     * @param angle angle of exit (normally -1 on creation)
+     * @param clockwise direction of flow for whirlpool
+     */
     public void addWPool(int x, int y, float size, float angle, int clockwise) {
     	Whirlpool whirl = new Whirlpool();
     	whirl.setCentreX(x);
